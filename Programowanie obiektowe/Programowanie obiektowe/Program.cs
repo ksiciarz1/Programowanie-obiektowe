db33dacaac867bfb8476ddb8b6f09405ca2ea4a1
kacper.siciarz <kacpersiciarz2001@gmail.com>
2022-03-10 11:36:13 +0100

    Dodanie kilka testów jednostkowych

--- Programowanie obiektowe/Programowanie obiektowe/Program.cs
+++ Programowanie obiektowe/Programowanie obiektowe/Program.cs
@@ -6,7 +6,14 @@ namespace Programowanie_obiektowe
     {
         static void Main(string[] args)
         {
+            Ulamek ulamek1 = new Ulamek(1, 2);
+            Ulamek ulamek2 = new Ulamek(3, 2);
 
+            Console.WriteLine(ulamek1 * ulamek1);
+            Console.WriteLine(ulamek1 + ulamek2);
+
+            Console.WriteLine(ulamek1.RoundUp);
+            Console.WriteLine(ulamek1.RoundDown);
         }
     }
 }
