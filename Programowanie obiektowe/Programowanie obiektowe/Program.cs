1a4d4569a406ec4017e7cf6da525fa07db60014b
kacper.siciarz <kacpersiciarz2001@gmail.com>
2022-03-11 08:35:37 +0100

    Dodano sortowwanie

--- Programowanie obiektowe/Programowanie obiektowe/Program.cs
+++ Programowanie obiektowe/Programowanie obiektowe/Program.cs
@@ -14,6 +14,43 @@ namespace Programowanie_obiektowe
 
             Console.WriteLine(ulamek1.RoundUp());
             Console.WriteLine(ulamek1.RoundDown());
+
+            Console.WriteLine("// Sortowanie tablicy");
+
+            Ulamek[] ulamekArray = new Ulamek[] { new Ulamek(5, 6), new Ulamek(1, 2), new Ulamek(4, 5), new Ulamek(6, 7) };
+
+            for (int i = 0; i < ulamekArray.Length; i++)
+            {
+                Console.WriteLine(ulamekArray[i]);
+            }
+
+            Console.WriteLine("// Posortowane");
+            ulamekArray = UlamekBubbleSort(ulamekArray);
+
+            for (int i = 0; i < ulamekArray.Length; i++)
+            {
+                Console.WriteLine(ulamekArray[i]);
+            }
+        }
+
+        public static Ulamek[] UlamekBubbleSort(Ulamek[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (j + 1 < array.Length)
+                    {
+                        if (array[j].CompareTo(array[j + 1]) > 0)
+                        {
+                            Ulamek temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            return array;
         }
     }
 }
