1af6fdb39025384104c7766e255cd56bddbaaa45
kacper.siciarz <kacpersiciarz2001@gmail.com>
2022-05-20 09:51:21 +0200

    lab8 - zadanie pt1

--- Programowanie obiektowe/lab8/Program.cs
+++ Programowanie obiektowe/lab8/Program.cs
@@ -9,11 +9,7 @@ namespace lab8
         static void Main(string[] args)
         {
             bool threadSuspend = false;
-            ISet<int> primeNumbers1 = new HashSet<int>();
-            ISet<int> primeNumbers2 = new HashSet<int>();
-            ISet<int> primeNumbers3 = new HashSet<int>();
-            ISet<int> primeNumbers4 = new HashSet<int>();
-            ISet<int> primeNumbers5 = new HashSet<int>();
+            ISet<int> primeNumbers = new HashSet<int>();
 
 
             // solution 1
@@ -22,32 +18,41 @@ namespace lab8
                 int number = 1;
                 while (!threadSuspend)
                 {
-                    lock (primeNumbers1)
+                    number++;
+                    Func<int, bool> func = new Func<int, bool>((number) =>
                     {
-                        number++;
                         int limit = (int)Math.Floor(Math.Sqrt(number));
                         for (int i = 2; i <= limit; ++i)
                         {
                             if (number % i == 0)
                             {
-                                continue;
+                                return false;
                             }
                         }
                         if (number >= 2)
-                            primeNumbers1.Add(number);
+                        {
+                            return true;
+                        }
+                        return false;
+                    });
+                    if (func(number))
+                    {
+                        lock (primeNumbers)
+                            primeNumbers.Add(number);
                     }
+
                 }
             });
 
             //solution 2
             Thread PrimeNumbersThread2 = new Thread(() =>
             {
-                int number = 1;
+                int number = 500;
                 while (!threadSuspend)
                 {
-                    lock (primeNumbers2)
+                    number++;
+                    Func<int, bool> func = new Func<int, bool>((number) =>
                     {
-                        number++;
                         int count = 0;
                         for (int i = 1; i <= number; ++i)
                         {
@@ -56,8 +61,13 @@ namespace lab8
                                 count += 1;
                             }
                         }
-                        if (count == 2)
-                            primeNumbers2.Add(number);
+                        return count == 2;
+                    });
+
+                    if (func(number))
+                    {
+                        lock (primeNumbers)
+                            primeNumbers.Add(number);
                     }
                 }
             });
@@ -65,33 +75,38 @@ namespace lab8
             //solution 3
             Thread PrimeNumbersThread3 = new Thread(() =>
             {
-                int number = 1;
+                int number = 1000;
                 while (!threadSuspend)
                 {
-                    lock (primeNumbers3)
+                    number++;
+                    Func<int, bool> func = new Func<int, bool>((number) =>
                     {
-                        number++;
                         if (number < 2)
                         {
-                            continue;
+                            return false;
                         }
                         if (number == 2)
                         {
-                            primeNumbers3.Add(number);
-                            continue;
+                            return true;
                         }
                         if (number % 2 == 0)
                         {
-                            continue;
+                            return false;
                         }
                         for (int i = 3; i < number; i += 2)
                         {
                             if (number % i == 0)
                             {
-                                continue;
+                                return false;
                             }
                         }
-                        primeNumbers3.Add(number);
+                        return true;
+                    });
+
+                    if (func(number))
+                    {
+                        lock (primeNumbers)
+                            primeNumbers.Add(number);
                     }
                 }
             });
@@ -99,91 +114,87 @@ namespace lab8
             //solution 4
             Thread PrimeNumbersThread4 = new Thread(() =>
             {
-                int number = 1;
+                int number = 1500;
                 while (!threadSuspend)
                 {
-                    lock (primeNumbers4)
+                    number++;
+                    if (number < 2)
                     {
-                        number++;
-                        if (number < 2)
-                        {
-                            continue;
-                        }
-                        if (number == 2)
-                        {
-                            primeNumbers4.Add(number);
-                            continue;
-                        }
-                        for (int i = 2; i < number; ++i)
-                        {
-                            if (number % i == 0)
-                            {
-                                continue;
-                            }
-                        }
-                        primeNumbers4.Add(number);
+                        continue;
                     }
+                    if (number == 2)
+                    {
+                        lock (primeNumbers)
+                            primeNumbers.Add(number);
+                        continue;
+                    }
+                    for (int i = 2; i < number; ++i)
+                    {
+                        if (number % i == 0)
+                        {
+                            break;
+                        }
+                    }
+                    lock (primeNumbers)
+                        primeNumbers.Add(number);
+
                 }
             });
 
             // solution 5
             Thread PrimeNumbersThread5 = new Thread(() =>
             {
-                int number = 1;
+                int number = 2000;
                 while (!threadSuspend)
                 {
-                    lock (primeNumbers5)
+                    number++;
+                    if (number < 2)
                     {
-                        number++;
-                        if (number < 2)
-                        {
-                            continue;
-                        }
-                        if (number == 2 || number == 3)
-                        {
-                            primeNumbers5.Add(number);
-                            continue;
-                        }
-                        if (number % 2 == 0)
-                        {
-                            continue;
-                        }
-
-                        int limit = (int)Math.Floor(Math.Sqrt(number));
-                        for (int i = 5, d = 4; i <= limit; i += d)
-                        {
-                            if (number % i == 0)
-                            {
-                                continue;
-                            }
-                            d = 6 - d;
-                        }
-                        primeNumbers5.Add(number);
+                        continue;
                     }
+                    if (number == 2 || number == 3)
+                    {
+                        lock (primeNumbers)
+                            primeNumbers.Add(number);
+                        continue;
+                    }
+                    if (number % 2 == 0)
+                    {
+                        continue;
+                    }
+
+                    int limit = (int)Math.Floor(Math.Sqrt(number));
+                    for (int i = 5, d = 4; i <= limit; i += d)
+                    {
+                        if (number % i == 0)
+                        {
+                            break;
+                        }
+                        d = 6 - d;
+                    }
+                    lock (primeNumbers)
+                        primeNumbers.Add(number);
                     continue;
                 }
             });
 
+
             PrimeNumbersThread1.Start();
-            PrimeNumbersThread2.Start();
-            PrimeNumbersThread3.Start();
-            PrimeNumbersThread4.Start();
-            PrimeNumbersThread5.Start();
+            //PrimeNumbersThread2.Start();
+            //PrimeNumbersThread3.Start();
+            //PrimeNumbersThread4.Start();
+            //PrimeNumbersThread5.Start();
 
             Thread.Sleep(10000);
             threadSuspend = true;
 
             PrimeNumbersThread1.Join();
-            PrimeNumbersThread2.Join();
-            PrimeNumbersThread3.Join();
-            PrimeNumbersThread4.Join();
-            PrimeNumbersThread5.Join();
+            //PrimeNumbersThread2.Join();
+            //PrimeNumbersThread3.Join();
+            //PrimeNumbersThread4.Join();
+            //PrimeNumbersThread5.Join();
 
-            Console.WriteLine($"Znaleziono w solution 1 {primeNumbers1.Count} liczb pierwszych");
-            Console.WriteLine($"Znaleziono w solution 2 {primeNumbers2.Count} liczb pierwszych");
-            Console.WriteLine($"Znaleziono w solution 3 {primeNumbers3.Count} liczb pierwszych");
-            Console.WriteLine($"Znaleziono w solution 4 {primeNumbers4.Count} liczb pierwszych");
-            Console.WriteLine($"Znaleziono w solution 5 {primeNumbers5.Count} liczb pierwszych");
+            Console.WriteLine($"Znaleziono {primeNumbers.Count} liczb pierwszych"); // All threads combined
         }
     }
 }
